bd104c1543c8d6bd29f6931c1a6592f3af24aebb
devendhiran0508 <devendhiran0508@gmail.com>
2025-06-18 16:06:20 +0530

    load the images

--- src/PacMan.java
+++ src/PacMan.java
@@ -9,9 +9,33 @@ public class PacMan extends JPanel {
     private int boardWidth = colCount * tileSize;
     private int boardHeight = rowCount * tileSize;
 
+    private Image wallImage;
+    private Image blueGhostImage;
+    private Image oranegGhostImage;
+    private Image pinkGhostImage;
+    private Image redGhostImage;
+    
+    private Image pacmanUpImage;
+    private Image pacmanDownImage;
+    private Image pacmanLeftImage;  
+    private Image pacmanRightImage;
+
+
     PacMan()
     {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+
+        //load the images
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        oranegGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+        //load pacman images
+        pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
+        pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
     }
 }
